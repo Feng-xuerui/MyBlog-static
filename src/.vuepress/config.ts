22d04c001b22385baef5fec1fcf6febe41513937
Feng-xuerui <33976094+Feng-xuerui@users.noreply.github.com>
2025-10-19 11:48:23 +0800

    更新base地址

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,9 +1,19 @@
+/*
+ * @Author: Feng-xuerui 33976094+Feng-xuerui@users.noreply.github.com
+ * @Date: 2025-10-18 17:10:56
+ * @LastEditors: Feng-xuerui 33976094+Feng-xuerui@users.noreply.github.com
+ * @LastEditTime: 2025-10-19 11:48:00
+ * @FilePath: \MyBlog-static\src\.vuepress\config.ts
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by ${git_name_email}, All Rights Reserved. 
+ */
 import { defineUserConfig } from "vuepress";
 
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/MyBlog-static/",
 
   lang: "zh-CN",
   title: "博客演示",
